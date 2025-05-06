fb685b278c285d5de720411be22ccdea6d07261d
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2025-05-06 22:25:58 +0700

    feat: add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
+
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
@@ -20,5 +22,6 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    netlifyPlugin()
   ],
 });
