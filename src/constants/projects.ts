6bf6bea46a011006a1fdcf5f668a13e6389bb682
Muhammad Difa Sulthon Diani <mdifasulthon@gmail.com>
2026-06-23 23:15:30 +0700

    feat: add new projects

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -4,24 +4,36 @@ import { Project } from "~/types/projects"
 const mobileProjects: Project[] = [
   {
     id: 1,
+    title: "Jenius",
+    desc: "Jenius is a digital banking application that allows users to manage their finances, perform transfers, and access debit and credit card services through a secure and intuitive mobile experience.",
+    image: "/images/porto_jenius.png"
+  },
+  {
+    id: 2,
+    title: "Jenius Daya",
+    desc: "Jenius Daya is a digital banking application designed for retirees, providing everyday banking services through a simplified and accessible user experience tailored to senior users.",
+    image: "/images/porto_jeniusdaya.png"
+  },
+  {
+    id: 3,
     title: "MyOto",
     desc: "MyOTO is an employee shuttle management application that facilitates transportation between company offices and work sites. The platform supports both passengers and drivers, route tracking, and transportation management features.",
     image: "/images/porto_myoto.png"
   },
   {
-    id: 2,
+    id: 4,
     title: "Buddyballs",
     desc: "BuddyBalls is a mobile application that connects football enthusiasts with nearby matches. Users can search games by date and location, book available slots, choose their playing position, and complete payments seamlessly within the app.",
     image: "/images/porto_buddyballs.png"
   },
   {
-    id: 2,
+    id: 5,
     title: "Smartstays",
     desc: "A custom Android TV platform for hotels that combines entertainment services, hotel information, and facility booking features into a single in-room guest experience.",
     image: "/images/porto_smartstays.png"
   },
   {
-    id: 3,
+    id: 6,
     title: "Bintang App",
     desc: "A digital asset management platform that centralizes magazine archives into a searchable mobile experience. The application enables users to discover, filter, and manage photos and media assets efficiently while maintaining structured content storage and metadata management.",
     image: "/images/porto_bintang.png"
